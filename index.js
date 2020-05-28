1894d7b76bf0cd8e98d118f17a5606156c377f07
ml-aakash-dev <ml.aakash.dev@gmail.com>
2020-05-29 02:25:24 +0500

    server

--- index.js
+++ index.js
@@ -8,46 +8,46 @@ const PORT = process.env.PORT || 3001
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/api/form', (req,res) => {
-    nodemailer.createTestAccount((err,account) => {
-        const htmlEmail = '<h3>Contact Details</h3>' +
-                            '<ul>' + 
-                                '<li>Name: ' + req.body.firstname + ' ' + req.body.lastname + '</li>' + 
-                                '<li>email: ' + req.body.email + '</li>' + 
-                            '</ul>' + 
-                            '<h3>Message</h3>' + 
-                            '<p>' + req.body.message + '</p>'
+// app.use('/api/form', (req,res) => {
+//     nodemailer.createTestAccount((err,account) => {
+//         const htmlEmail = '<h3>Contact Details</h3>' +
+//                             '<ul>' + 
+//                                 '<li>Name: ' + req.body.firstname + ' ' + req.body.lastname + '</li>' + 
+//                                 '<li>email: ' + req.body.email + '</li>' + 
+//                             '</ul>' + 
+//                             '<h3>Message</h3>' + 
+//                             '<p>' + req.body.message + '</p>'
 
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            host: 'smtp.gmail.com',
-            auth: {
-                user: 'ml.aakash.dev@gmail.com',
-                pass: 'ml.aakash786'
-            }
-        })
+//         let transporter = nodemailer.createTransport({
+//             service: 'gmail',
+//             host: 'smtp.gmail.com',
+//             auth: {
+//                 user: 'ml.aakash.dev@gmail.com',
+//                 pass: 'ml.aakash786'
+//             }
+//         })
 
-        let mailOptions = {
-          from: req.body.email,
-          to: 'ml.aakash.dev@gmail.com',
-          replyTo: req.body.email,
-          subject: 'New Message',
-          html: htmlEmail
-        };
+//         let mailOptions = {
+//           from: req.body.email,
+//           to: 'ml.aakash.dev@gmail.com',
+//           replyTo: req.body.email,
+//           subject: 'New Message',
+//           html: htmlEmail
+//         };
         
-        transporter.sendMail(mailOptions, (err, data) => {
-            if (err) {
-              res.json({
-                msg: 'fail'
-              })
-            } else {
-              res.json({
-                msg: 'success'
-              })
-            }
-          }); 
-    })
-})
+//         transporter.sendMail(mailOptions, (err, data) => {
+//             if (err) {
+//               res.json({
+//                 msg: 'fail'
+//               })
+//             } else {
+//               res.json({
+//                 msg: 'success'
+//               })
+//             }
+//           }); 
+//     })
+// })
 
 //serve static assets if in production
 if(process.env.NODE_ENV === 'production'){
